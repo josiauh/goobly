b6de5c86a576610fb35fae39cef862b3907e4fcf
Mar10Josh <65928867+Mar10Josh@users.noreply.github.com>
2022-02-10 21:29:54 -1000

    Update index.js

--- index.js
+++ index.js
@@ -33,8 +33,10 @@ function tocsp(sec) {
 const os = require("os")
 // FS was here, but I don't need it.
 const io = require("socket.io-client");
-
+// VM was also here but we don't need to run isolations
 const cfg = JSON.parse(require('./cfg.json'));
+
+const tagt = '<style/confuse=node/html=parser/amogus>\n/* This is Taggy\'s Tags. */ nomsg { color: transparent; }\n matrix { color: green; background-color: black; }  '
 
 const client = io("https://rmtrollbox.eu-gb.mybluemix.net/", {
   "force new": true
@@ -42,4 +44,14 @@ const client = io("https://rmtrollbox.eu-gb.mybluemix.net/", {
 
 client.on('connect', function(data) {
   client.emit('user joined', cfg.name + "[" + cfg.prefix + "]", cfg.color)
+  client.send("Taggy, Manager for Tags and The Box!")
+  client.send("Now Managing the tags.")
+  client.send(tagt);
+  client.send("Tags managed! So far we have:\nnomsg\nmatrix.");
+})
+
+client.on('message', function(data) {
+   if (data.msg.startsWith(cfg.prefix)) {
+      client.send("Hold my Code! The commands are not ready yet! Slow down!")
+   }
 })
